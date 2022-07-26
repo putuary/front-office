6eeacb4c05b919e7ccc2bf9a7d56bf92db6909eb
Putu Ary Kusuma Yudha <82919534+putuary@users.noreply.github.com>
2022-07-26 09:10:06 +0700

    Delete Front-end directory

--- Front-end/app/Helper/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-function rupiah($angka){
-	
-	$hasil_rupiah = "Rp. " . number_format($angka,0,',','.');
-	echo $hasil_rupiah;
- 
-}
--- Front-end/config/constant.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'ip' => '192.168.1.102',
-    'url' => 'http://laravel-9-restful-api-master.test',
-    'password' => '12345678'
-];
